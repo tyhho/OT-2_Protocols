3acd01b2b53e50691b54dfe890bd56debd8d37e2
Trevor Y. H. Ho <trevor.ho@ed.ac.uk>
2019-08-05 20:01:28 +0100

    new code for carrying out dilution of overnight culture into medium with 4 different induction conditions

---
+++ ot2code_4ConInduction.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  4 20:27:10 2019
+
+@author: s1635543
+"""
+
+from opentrons import labware, instruments,robot
+
+#%%
+def distributeNoBlowOutLite(pipette,vol_in,vol_out,source,dests):
+    pipette.pick_up_tip()
+    pipette.aspirate(vol_in,source)
+    for dest in dests:
+        pipette.dispense(vol_out,dest)
+    pipette.drop_tip()
+
+#%%
+culture_vol = 2
+
+slots_map = {
+        '1':'96-flat',
+        '2':'96-flat',
+        '3':'96-flat',
+        '5':'96-flat',
+        '6':'96-flat'
+        }
+
+labware_items = {}
+for slot, labware_item in slots_map.items():
+    labware_items.update({slot:labware.load(labware_item, slot)})
+
+tip_slots = ['10','11']
+tip_racks = [labware.load('tiprack-10ul-custom', slot) for slot in tip_slots]
+
+p10m = instruments.P10_Multi(
+    mount='right',
+    tip_racks=tip_racks
+    )
+
+#%%
+
+for col_index in range(len(labware_items['1'].cols())):
+    distributeNoBlowOutLite(p10m,
+                            (culture_vol*2+2),
+                            culture_vol,
+                            labware_items['1'].cols(col_index),
+                            [labware_items['2'].cols(col_index),labware_items['3'].cols(col_index)]
+                            )
+for col_index in range(len(labware_items['1'].cols())):
+    distributeNoBlowOutLite(p10m,
+                            (culture_vol*2+2),
+                            culture_vol,
+                            labware_items['1'].cols(col_index),
+                            [labware_items['5'].cols(col_index),labware_items['6'].cols(col_index)]
+                            )
+for c in robot.commands():
+    print(c)
