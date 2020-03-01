2b8d21b8e1f01e9b8c8ae29d679f7137f1f748e3
Trevor Y. H. Ho <trevor.ho@ed.ac.uk>
2020-03-01 23:13:10 +0000

    Starting update for tiprack test

---
+++ ot2code_CustomTipRack10uLTest.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  1 23:03:25 2020
+
+@author: Trevor Ho
+"""
+
+# Import libraries for OT-2
+from opentrons import labware, instruments,robot
+
+# Put plates and racks onto the deck
+slots_map = {
+        '2':'nest_96_wellplate_100ul_pcr_full_skirt',
+        }
+
+deck_labware = {}
+for slot, labware_item in slots_map.items():
+    deck_labware.update({slot:labware.load(labware_item, slot)})
+
+# Put tip boxes onto the deck
+
+tip_slots_10 = ['1']
+tip_racks_10 = []
+for slot in tip_slots_10:
+        tip_racks_10.append(labware.load('geb_96_tiprack_10ul', slot))
+
+# Configure the pipettes
+
+p10m = instruments.P10_Multi(
+    mount='left',
+    tip_racks=tip_racks_10
+    )
+
+# Set up the pipetting instruction
+
+#%%    
+## Execution of instructions
+
+p10m.pick_up_tip()
+for i in range(11):
+    p10m.transfer(
+        10,
+        deck_labware['2'].cols(i),
+        deck_labware['2'].cols(i+1),
+        new_tip = 'never',
+        blow_out = False)
+
+p10m.drop_tip()
+    
+# Print out the commands step by step
+for c in robot.commands():
+    print(c)
